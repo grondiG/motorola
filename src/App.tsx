1781cae6b8617bfe577f4d83badb93afa048360e
grondi <arogrondi@gmail.com>
2023-03-05 23:59:05 +0100

    fix of proteiningo

--- src/App.tsx
+++ src/App.tsx
@@ -23,18 +23,18 @@ function App() {
 
   const getSequence = (seq: string) => {
     for(let i=0;i<seq.length;i++){
-        if(seq.slice(i,i+3) === "AUG" || seq.slice(i,i+3) === "ATG"){
-            seq = seq.slice(i);
-            break;
-        }
+      if(seq.slice(i,i+3) === "AUG" || seq.slice(i,i+3) === "ATG"){
+        seq = seq.slice(i);
+        break;
+      }
     }
     seq = seq.replaceAll("T","U");
     console.log(minSequenceLength);
 
     if(seq.length/3<minSequenceLength){
-        toast.error("Zbyt krótka sekwencja białka względem minimalnej wartości");
-        setIsSubmited(false);
-        return;
+      toast.error("Zbyt krótka sekwencja białka względem minimalnej wartości");
+      setIsSubmited(false);
+      return;
     }
 
     const split = seq.match(/(AUG(?:[AUGC]{3,3})*?(?:UAG|UAA|UGA))/g) || [];
@@ -61,19 +61,12 @@ function App() {
     axios.get(`/api/sequences/${seq}`).then((response) => {
       setProteinInfo(response.data.sequences);
       toast.dismiss();
-      }).catch(e=>{
+    }).catch(e=>{
       console.log(e);
     });
-    
+
   }
 
-  useEffect(() => {
-    scrollTo({
-        top: 0,
-        left: 0,
-        behavior: 'smooth',
-      });
-  },[]);
 
   useEffect(() => {
     setProteinInfo([]);
@@ -88,54 +81,56 @@ function App() {
   }
 
   return (
-    <>
-      <div className='w-screen h-screen grid grid-cols-2 grid-rows-1'>
-        <div className='h-full w-full relative'>
-          <div className='relative top-[30%] right-10'>
-            <h1 className='text-5xl font-bold text-right'>
-              Wprowadz sekwencje bialek{''}
-              <span className='leading-tight font-mono block text-right font-extrabold text-transparent text-8xl bg-clip-text bg-gradient-to-r from-purple-800 to-purple-400'>
+      <>
+        <div className='w-screen h-screen grid grid-cols-2 grid-rows-1'>
+          <div className='h-full w-full relative'>
+            <div className='relative top-[30%] right-10'>
+              <h1 className='text-5xl font-bold text-right'>
+                Wprowadz sekwencje bialek{''}
+                <span className='leading-tight font-mono block text-right font-extrabold text-transparent text-8xl bg-clip-text bg-gradient-to-r from-purple-800 to-purple-400'>
                 {type || 'RNA'}
               </span>
-            </h1>
-            <AcidType sequence={sequence} type={type} setType={setType} setSequence={setSequence} />
-            <InputField
-              sequence={sequence}
-              setSequence={setSequence}
-              type={type}
-              setIsSubmited={setIsSubmited}
-              minInput={minInput}
-            />
-            <ButtonContainer
-              sequence={sequence}
-              setSequence={setSequence}
-              setIsSubmited={setIsSubmited}
-            />
+              </h1>
+              <AcidType sequence={sequence} type={type} setType={setType} setSequence={setSequence} />
+              <InputField
+                  sequence={sequence}
+                  setSequence={setSequence}
+                  type={type}
+                  setIsSubmited={setIsSubmited}
+                  minInput={minInput}
+              />
+              <ButtonContainer
+                  sequence={sequence}
+                  setSequence={setSequence}
+                  setIsSubmited={setIsSubmited}
+              />
+            </div>
           </div>
-        </div>
 
-        <InputCaption />
-        <input ref={minInput} value={minSequenceLength} type="number" className="minAminoAcidCount" min="2" defaultValue={100} step={1} onInput={(event:any)=>{
-          event.target.value = event.target.value.replace(/[^0-9]/g, '');
-          setMinSequenceLength(parseInt(event.target.value));
-        }} title="Minimalna wartość ilości aminokwasów w białku (domyślnie 100)" placeholder="Minimalna wartość ilości aminokwasów w białku (domyślnie 100)"/>
-        <Canvas className='h-full w-full'>
-          <Rna seq={sequence} />
-        </Canvas>
-      </div>
-      <div className={`result`}>
-      {isSubmited && (
-        splitSequence.map((seq, index) => {
-          console.log(proteinInfo[index]?.sequence);
-          return (
-              <Result seq={proteinInfo[index]?.sequence} setIsSubmited={setIsSubmited} isSubmited={isSubmited} index={index}
-              proteinInfo={proteinInfo[index]} length={splitSequence.length}/>
-          )
-        }))
-      }
-      </div>
-      <ToastContainer theme="dark" />
-    </>
+          <InputCaption />
+          <input ref={minInput} value={minSequenceLength} type="number" className="minAminoAcidCount" min="2" defaultValue={100} step={1} onInput={(event:any)=>{
+            event.target.value = event.target.value.replace(/[^0-9]/g, '');
+            setMinSequenceLength(parseInt(event.target.value));
+          }} title="Minimalna wartość ilości aminokwasów w białku (domyślnie 100)" placeholder="Minimalna wartość ilości aminokwasów w białku (domyślnie 100)"/>
+          <Canvas className='h-full w-full'>
+            <Rna seq={sequence} />
+          </Canvas>
+        </div>
+        <div className={`result`}>
+          {isSubmited && (
+              splitSequence.map((seq, index) => {
+                if(proteinInfo.length===0){
+                  return ;
+                }
+                return (
+                    <Result seq={proteinInfo[index]?.sequence} setIsSubmited={setIsSubmited} isSubmited={isSubmited} index={index}
+                            proteinInfo={proteinInfo[index]} length={splitSequence.length}/>
+                )
+              }))
+          }
+        </div>
+        <ToastContainer theme="dark" />
+      </>
   );
 }
 
